0eb5e69c8fd33bff982c760ce61da3bd6a5821f5
scu-wcc <1047532566@qq.com>
2025-01-06 03:30:14 +0800

    2025-01-05

--- basic_code/day02/src/scu/wcc/demo1/HomeWork.java
+++ basic_code/day02/src/scu/wcc/demo1/HomeWork.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("C");
+        System.out.println("D");
         System.out.println("19.5");
         double bankBalance = 500.00;
         bankBalance = bankBalance + 250;
@@ -12,7 +12,7 @@ public class HomeWork {
         System.out.println("小明账户存款："+bankBalance);
 
         System.out.println("C D E F");
-        System.out.println("BD, AD, C, ");
+        System.out.println("BD, A, C, ");
 
         String name;
         String birthday;

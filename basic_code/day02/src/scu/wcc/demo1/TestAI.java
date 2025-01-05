c2e7a93c971eaa9af2a7303ebdcb2c7747090731
scu-wcc <1047532566@qq.com>
2025-01-06 02:37:11 +0800

    2025-01-05

---
+++ basic_code/day02/src/scu/wcc/demo1/TestAI.java
@@ -0,0 +1,15 @@
+package scu.wcc.demo1;
+
+public class TestAI {
+    public static void main(String[] args) {
+        // 定义一个整数记录整数10
+        int num = 10;
+        // 定义一个整数记录整数20
+        int num2 = 20;
+        // 将两数相加
+        int sum = num + num2;
+        // 打印结果
+        System.out.println("两数之和为：" + sum);
+        System.out.println("HelloWorld!");
+    }
+}

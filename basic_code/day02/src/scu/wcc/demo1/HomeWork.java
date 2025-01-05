a21d1d30fb61ad29e0a3de215514ed045ad32d51
scu-wcc <1047532566@qq.com>
2025-01-06 03:19:27 +0800

    2025-01-05

---
+++ basic_code/day02/src/scu/wcc/demo1/HomeWork.java
@@ -0,0 +1,33 @@
+package scu.wcc.demo1;
+
+import java.sql.SQLOutput;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("C");
+        System.out.println("19.5");
+        double bankBalance = 500.00;
+        bankBalance = bankBalance + 250;
+        bankBalance = bankBalance - 100;
+        System.out.println("小明账户存款："+bankBalance);
+
+        System.out.println("C D E F");
+        System.out.println("BD, AD, C, ");
+
+        String name;
+        String birthday;
+        int age;
+        float money;
+
+        // 八大基本数据类型
+        byte b = 10;
+        short s = 20;
+        int i = 100;
+        long l = 1000L;
+        char c = 'C';
+        boolean flat= true;
+        double d = 12.000;
+        float f = 12.000F;
+
+    }
+}

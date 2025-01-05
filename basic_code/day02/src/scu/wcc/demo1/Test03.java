68fa5b9cf62d21c78fc571f416df96474d961388
scu-wcc <1047532566@qq.com>
2025-01-06 03:30:24 +0800

    2025-01-05

---
+++ basic_code/day02/src/scu/wcc/demo1/Test03.java
@@ -0,0 +1,26 @@
+package scu.wcc.demo1;
+
+public class Test03 {
+    public static void main(String[] args){
+
+        // 八大基本数据类型
+        byte b = 10;
+        short s = 20;
+        int i = 100;
+        long l = 1000L;
+        char c = 'C';
+        boolean flat= true;
+        double d = 12.000;
+        float f = 12.000F;
+        String name = "王某某";
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(flat);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(name);
+    }
+}
